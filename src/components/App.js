c5d8f4143628e2eba1c165d3f91db30e401c0180
Collus Watson <cklwatsonjr@gmail.com>
2022-04-26 20:36:56 -0400

    create components

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+import Extra from './Extra';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Home />
+      <Extra />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Extra.js
@@ -0,0 +1,9 @@
+const Extra = () => {
+    return ( 
+        <div>
+            <h3>Extras</h3>
+        </div>
+     );
+}
+ 
+export default Extra;
